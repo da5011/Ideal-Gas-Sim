2acdc04ffebe6ff098eaacb8a5d8e5f2a85a067e
da5011 <davidevan3605@gmail.com>
2023-04-13 13:04:37 -0700

    initialized position

--- sim.f90
+++ sim.f90
@@ -8,11 +8,11 @@ program sim
     R = 8.314
 
     open(stdin, file = "input.txt", action = "read")
-    read(*) n
-    read(*) T
-    read(*) m
-    read(*) bx
-    read(*) by
+    read(stdin, *) n
+    read(stdin, *) T
+    read(stdin, *) m
+    read(stdin, *) bx
+    read(stdin, *) by
 
     !Want to initialize by randomizing positions of all atoms
     !and their velocities such that the average corresponds with the temperature
@@ -23,12 +23,12 @@ program sim
     allocate(vy(n))
 
     open(stdout, file = "output.txt", action = "write")
-    write(*) "Populating box and setting intial velocities..."
+    write(stdout, *) "Populating box and setting intial velocities..."
     do i = 1, n
         x(i) = rand()*bx
-        write(x(i))
+        write(stdout, *) x(i)
         y(i) = rand()*by
-        write(y(i))
+        write(stdout, *) y(i)
     enddo
 
 end program sim
